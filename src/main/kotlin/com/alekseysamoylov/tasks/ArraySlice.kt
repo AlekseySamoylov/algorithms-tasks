5ad2aaae110951610337944b41863f57699306b7
Aleksey Samoylov <aleksey.samoylov@moneyman.ru>
2019-10-25 16:57:11 +0400

    refactoring

--- src/main/kotlin/com/alekseysamoylov/tasks/ArraySlice.kt
+++ src/main/kotlin/com/alekseysamoylov/tasks/ArraySlice.kt
@@ -1,15 +1,73 @@
 package com.alekseysamoylov.tasks
 
-import java.util.concurrent.CopyOnWriteArrayList
+import kotlin.math.max
 
 
 class ArraySlice {
     fun solution(A: IntArray): Int {
+        var maxSum: Int = A[0]
+        var previousSum = 0
+        A.forEach { element ->
+            val current = previousSum + element
+            previousSum = when {
+                current > 0 -> current
+                else -> 0
+            }
+            maxSum = max(maxSum, current)
+        }
+        return maxSum
+    }
+
+/*
+    fun badSolution(A: IntArray): Int {
         val resultList: MutableList<IntWrap> = CopyOnWriteArrayList()
         if (A.size == 1) {
             return A[0]
         }
-        A.forEach { arrayElement ->
+        val maximalElement = A.max()
+        if (maximalElement!! <= 0) {
+            return maximalElement
+        }
+
+        val simplifiedA: MutableList<Int> = mutableListOf()
+        var sumOfNegativeSequence = 0
+        var sumOfPositiveSequence = 0
+        var previousElement = 0
+        A.forEach {
+            if (it < 0) {
+                if (sumOfPositiveSequence > 0) {
+                    simplifiedA.add(sumOfPositiveSequence)
+                    sumOfPositiveSequence = 0
+                    previousElement = it
+                }
+
+                if (sumOfNegativeSequence == 0) {
+                    sumOfNegativeSequence = it
+                    previousElement = it
+                } else {
+                    if (previousElement < it) {
+                        sumOfNegativeSequence += it
+                        previousElement = it
+                    }
+                }
+            } else {
+                if (sumOfNegativeSequence < 0) {
+                    simplifiedA.add(sumOfNegativeSequence)
+                    sumOfNegativeSequence = 0
+                    sumOfPositiveSequence += it
+                } else {
+                    sumOfPositiveSequence += it
+                    previousElement = it
+                }
+            }
+        }
+        if (sumOfNegativeSequence != 0) {
+            simplifiedA.add(sumOfNegativeSequence)
+        } else if (sumOfPositiveSequence != 0) {
+            simplifiedA.add(sumOfPositiveSequence)
+        }
+
+        simplifiedA.forEach { arrayElement ->
             var createNewResultArrayForNextPositive = false
             if (resultList.size > 0) {
                 for (i in resultList.indices) {
@@ -41,7 +99,7 @@ class ArraySlice {
         }
 
         return resultList.maxBy { it.slice }!!.slice
-    }
+    }*/
 
     data class IntWrap(var slice: Int, var finished: Boolean = false)
 }
--- src/test/kotlin/com/alekseysamoylov/tasks/ArraySliceTest.kt
+++ src/test/kotlin/com/alekseysamoylov/tasks/ArraySliceTest.kt
@@ -33,20 +33,42 @@ internal class ArraySliceTest {
 //        println(array[array.size -1])
 //    }
 
+//    @Test
+//    fun continueTest() {
+//        var counter = 1
+//        var array = IntArray(1000) { counter++ }
+//        var result = 0
+//        for (i in array) {
+//            result += i
+//        }
+//        println(result)
+//    }
+
+    // 3, -15, 21, -6, 105, -6, 4
     companion object {
         var counter = -99
+        var secondCounter = 1
 
         @JvmStatic
         fun squares() = listOf(
             Arguments.of(intArrayOf(3,2,-1000,2,-6,4,992,-2,4,0,2,-6,4,0,2,-6,4,99,2,-6,4,0), 1099),
+            Arguments.of(intArrayOf(3,-5,-4,-3,-2,-1,0,8, 7, 6,-3, -2, -1,4,99,2,-6,4,0), 120),
             Arguments.of(intArrayOf(3,2,-6,4,0,2,-6,4,0,2,-6,4,99,2,-6,4,0), 105),
             Arguments.of(intArrayOf(3,2,-6,4,0), 5),
             Arguments.of(intArrayOf(3,2,-6,4,0), 5),
             Arguments.of(intArrayOf(-3,5), 5),
+            Arguments.of(intArrayOf(0,0,1), 1),
+            Arguments.of(intArrayOf(0,0,-1), 0),
+            Arguments.of(intArrayOf(1,-2,3), 3),
+            Arguments.of(intArrayOf(1,2,3), 6),
+            Arguments.of(intArrayOf(-1,-2,-3), -1),
+            Arguments.of(intArrayOf(3,2,1), 6),
             Arguments.of(intArrayOf(3,-5,-8), 3),
             Arguments.of(intArrayOf(8,-5,7), 10),
-//            Arguments.of( IntArray(99) { counter++ }, -1),
-            Arguments.of(intArrayOf(-10,-5,-2), -2)
+            Arguments.of( IntArray(99) { counter++ }, -1),
+            Arguments.of( IntArray(1000) { secondCounter++ }, 500500),
+            Arguments.of(intArrayOf(-10,-5,-2), -2),
+            Arguments.of(intArrayOf(1,-10,-5,-2), 1)
         )
     }
 }
